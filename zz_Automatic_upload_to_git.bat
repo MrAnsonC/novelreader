4beedace4a3c905de1da86863c7aebc5c4e428b3
HSC <g3gt26@gmail.com>
2024-07-28 19:43:29 +1200

    bat update

--- zz_Automatic_upload_to_git.bat
+++ zz_Automatic_upload_to_git.bat
@@ -2,8 +2,8 @@
 echo Choose a commit message:
 echo 1. index page update
 echo 2. reading page update
-echo 3. novel update
-echo 4. Enter other message
+echo 3. 小说更新
+echo 4. 其他信息？
 
 
 :Prompt
@@ -12,10 +12,9 @@ set /p choice="选择以下哪里更新/新增: "
 if "%choice%"=="1" set "message=index page update"
 if "%choice%"=="2" set "message=reading page update"
 if "%choice%"=="3" goto NovelChoice
-if "%choice%"=="4" set /p message="Enter your custom commit message: "
+if "%choice%"=="4" set /p message="输入Commit的信息: "
 
 goto Continue
-
 :NovelChoice
 echo 选择以下那本小说更新/新增:
 echo 1. 守水库？我守的是时间长河！
@@ -25,7 +24,7 @@ set /p novelChoice="选择以下那本小说更新/新增: "
 if "%novelChoice%"=="1" set "message=守水库？我守的是时间长河！ 小说更新啦！"
 if "%novelChoice%"=="2" set "message=从未来归来的工藤柯南 小说更新啦"
 if "%novelChoice%"=="3" (
-    set /p message="Enter the new novel name: "
+    set /p message="新增小说名字： "
     set "message=新增小说：%novelName%"
 )
 goto Continue
