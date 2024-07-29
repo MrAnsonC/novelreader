9d14dda84e8f371d31ab57ac08e2b30781b7fe24
HSC <g3gt26@gmail.com>
2024-07-29 22:39:17 +1200

    BAT update

--- zz_Automatic_upload_to_git.bat
+++ zz_Automatic_upload_to_git.bat
@@ -17,14 +17,14 @@ if "%choice%"=="4" set /p message="输入Commit的信息: "
 goto Continue
 :NovelChoice
 echo 选择以下那本小说更新/新增:
+echo 0. 新小说（输入）
 echo 1. 守水库？我守的是时间长河！
 echo 2. 从未来归来的工藤柯南
 echo 3. 人在柯南，有脑内选项系统
-echo 0. 新小说（输入）
 
 set /p novelChoice="选择以下那本小说更新/新增: "
 if "%novelChoice%"=="0" (
-    set /p message="新增小说名字： "
+    set /p novelName="新增小说名字： "
     set "message=新增小说：%novelName%"
 )
 if "%novelChoice%"=="1" set "message=守水库？我守的是时间长河！ 小说更新啦！"
