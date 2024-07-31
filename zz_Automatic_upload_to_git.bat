cb6356781173e5028f0ed9be77cae50d65904e3e
HSC <g3gt26@gmail.com>
2024-07-31 16:10:53 +1200

    新小说： Test and update bat

--- zz_Automatic_upload_to_git.bat
+++ zz_Automatic_upload_to_git.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Choose a commit message:
-echo 1. index page update
-echo 2. reading page update
+echo 1. index page更新
+echo 2. reading page更新
 echo 3. 小说更新
 echo 4. 其他信息？
 
@@ -15,6 +15,7 @@ if "%choice%"=="3" goto NovelChoice
 if "%choice%"=="4" set /p message="输入Commit的信息: "
 
 goto Continue
+
 :NovelChoice
 echo 选择以下那本小说更新/新增:
 echo 0. 新小说（输入）
@@ -23,15 +24,16 @@ echo 2. 从未来归来的工藤柯南
 echo 3. 人在柯南，有脑内选项系统
 
 set /p novelChoice="选择以下那本小说更新/新增: "
-if "%novelChoice%"=="0" (
-    set /p novelName="新增小说名字： "
-    set "message=新增小说：%novelName%"
-)
+if "%novelChoice%"=="0" goto AddNovel
 if "%novelChoice%"=="1" set "message=守水库？我守的是时间长河！ 小说更新啦！"
 if "%novelChoice%"=="2" set "message=从未来归来的工藤柯南 小说更新啦"
 if "%novelChoice%"=="3" set "message=人在柯南，有脑内选项系统 小说更新啦"
 
 goto Continue
+
+:AddNovel
+set /p entermessage="输入小说名称: "
+set "message=新小说： %entermessage%"
 
 :Continue
 cd %CD%
